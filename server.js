1a33a644736ac6a9fe0372a80d7f0afbd46681e6
MindSparkTm <smartsurajit2008@gmail.com>
2017-10-16 07:26:25 -0700

    second commit

--- server.js
+++ server.js
@@ -1,81 +1,96 @@
-/**
- * Created by user on 08-01-2017.
- */
-const express = require('express');
-const bodyParser= require('body-parser');
-const app = express();
+var express = require("express");
+var multer = require('multer')
+var app = express()
+var path = require('path')
+
+var ejs = require('ejs')
+app.set('view engine', 'ejs')
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
 var db;
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-dirname = "C:/Users/user/WebstormProjects/untitled/public";
-const MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
-
-MongoClient.connect('mongodb://raaj:mom12345@ds145868.mlab.com:45868/employee', (err, database) => {
+MongoClient.connect(url, (err, database) => {
     if (err) return console.log(err)
-    db = database
-    app.listen(3000, () => {
-    console.log('listening on 3000')
+
+    database.createCollection("imageinfo", function(err, res) {
+        if (err) throw err;
+        console.log("Collection created!");
+        database.close();
+    });
+    db = database;
+});
+
+
+
+app.get('/api/file', function(req, res) {
+    res.render('pages/upload.ejs')
 })
-});
 
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './public/Images')
+    },
+    filename: function(req, file, callback) {
+        callback(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
 
-
-
-
-app.get('/id',function(req,res){
-    var c = req.query.name;
-    db.collection("quotes").findOne({name: new ObjectId(c)},
-        function(err, res) {
-
-            if (err) console.log(err);
-
-            if(res!=null){
-                console.log(res)
-                return false;
+app.post('/api/file', function(req, res) {
+    var upload = multer({
+        storage: storage,
+        fileFilter: function(req, file, callback) {
+            var ext = path.extname(file.originalname)
+            console.log(file.originalname);
+            if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+                return callback(res.end('Only images are allowed'), null)
             }
-
-            if(res==null){
-                return false;
-            }
-
-        });
-
-});
-
-app.get('/name',function(req,res){
-    var c = req.query.name;
-    db.collection("quotes").find({"name":c}).toArray(function(err,docs){
-        if(!err){
-            console.log(docs);
+            callback(null, true)
+            insertDocument(db, file.originalname, function () {
+                db.close();
+            });
         }
+    }).single('userFile');
+    upload(req, res, function(err) {
+        res.end('File is uploaded')
+
+
     });
 
 
 });
 
 
+app.get("/fetchrecords",function(req,res){
+    console.log("entered");
 
-app.get('/', (req, res) => {
-    db.collection('quotes').find().toArray((err, result) => {
-    if (err) return console.log(err)
-        var s = [];
-    // renders index.ejs
-        for(var i=0;i<result.length;i++){
-        console.log(result[i].name);
-        s.push(result[i].name);
+    findImages(db, function() {
+        db.close();
+    });
+
+});
+var insertDocument = function(db,filename, callback) {
+    db.collection('imageinfo').insertOne( {
+         myobj:{"imagename":filename}
+
+}, function(err, result) {
+        console.log("Inserted a document into the image collection.");
+
+
+    });
+};
+
+var findImages = function(db, callback) {
+    var cursor =db.collection('imageinfo').find( );
+    cursor.each(function(err, doc) {
+        if (doc != null) {
+            console.dir("doc",doc);
+        } else {
+            callback();
         }
-    res.render('pages/home.ejs', {quotes: s})
-})
-})
+    });
 
-app.post('/quotes', (req, res) => {
-    db.collection('quotes').save(req.body, (err, result) => {
-    if (err) return console.log(err)
+};
 
-    console.log('saved to database')
-res.redirect('/')
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port)
 })
-})
-
-
